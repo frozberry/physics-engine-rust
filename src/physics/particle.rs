05fe70c142db74158d4fcba2f0205f3ad2337bf6
frozberry <93860959+frozberry@users.noreply.github.com>
2022-08-17 10:35:09 +0100

    Implemenet inverse mass

--- src/physics/particle.rs
+++ src/physics/particle.rs
@@ -5,24 +5,27 @@ pub struct Particle {
     pub vel: Vec2,
     pub acc: Vec2,
     pub mass: f32,
+    pub inv_mass: f32,
     pub radius: i16,
     pub net_force: Vec2,
 }
 
 impl Particle {
     pub fn new(x: f32, y: f32, mass: f32, radius: i16) -> Self {
+        let inv_mass = if mass > 0. { 1. / mass } else { 0. };
         Particle {
             pos: Vec2::new(x, y),
             vel: Vec2::new(0., 0.),
             acc: Vec2::new(0., 0.),
             mass,
+            inv_mass,
             radius,
             net_force: Vec2::new(0., 0.),
         }
     }
 
     pub fn integrate(&mut self, dt: f32) {
-        self.acc = self.net_force / self.mass;
+        self.acc = self.net_force * self.inv_mass;
         self.vel += self.acc * dt;
         self.pos += self.vel * dt;
     }
